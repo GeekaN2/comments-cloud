b95ac3cbf478d3fdf3f464214182717748018cef
GeekaN <geekan@bk.ru>
2019-10-15 12:44:57 +0300

    CRD
    Functions for creating, reading, deleting are implemented.
    Also added several integration tests.
    Deleted useless files

--- cloud/app.js
+++ cloud/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 
 var indexRouter = require('./routes/index');
-var consoleRouter = require('./routes/console')
 
 var app = express();
 
@@ -22,10 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// TODO: indexRouter
-// app.use('/', indexRouter);
-app.use('/', consoleRouter);
-
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- cloud/scripts/mongodb_functions.js
+++ cloud/scripts/mongodb_functions.js
@@ -1,32 +1,13 @@
 const MongoClient = require("mongodb").MongoClient;
- 
+const fs = require('fs');
 // Initialize MongoClient
-const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true, useUnifiedTopology: true });
-mongoClient.connect(function(err, client){
- 
-    if(err) {
-        return console.log(err);
-    }
-    
-    var db = client.db('AUMD9ksWu1E0e44d6ZDUAvR1h3kLq9'); // API key
-    
-    var collection = db.collection("url_or_something_else"); // entity name in application
+const mongoKey = fs.readFileSync('env/mongodb_key.txt', 'utf-8').toString();
 
-    const comment = { 
-        username: 'Geekon',
-        text: 'Super text',
-        parent_id: null,
-        date: new Date(),
-        rating: 0
-     };
-    /*collection.insert(comment, (err, result) => {
-        if (err) { 
-            console.log(err);
-        } else {
-            console.log(result.ops[0]);
-        }
-    });*/
-    console.log(collection);
+const mongoClient = new MongoClient(mongoKey, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+mongoClient.connect(function(err, client){
+    if (err) throw err;
 
     client.close();
 });
